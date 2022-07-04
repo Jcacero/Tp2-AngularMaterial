75a7c0902d991afa175cd714e056f41a7557ee01
Juan Caceres <jcruz2013c@gmail.com>
2022-07-04 08:47:35 -0300

    ultimosa arreglos

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -19,13 +19,13 @@ export class CardsComponent implements OnInit {
       },
       {
         titulo: 'Remeras',
-        imagen: 'https://scontent.faep9-2.fna.fbcdn.net/v/t39.30808-6/242192348_1057916521686621_4794815972933518487_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeFO6v3cbKrwpL0GfyaxlbOCvKEoB1-aakW8oSgHX5pqRRgPgxaV9iVTbTHZ6aMFK8lukTTk2xLXGYU8JDLk1DVZ&_nc_ohc=a9d5qkPgPPMAX_v-EcG&_nc_oc=AQklxyfnhM-OJIQpIz8vmKpbDV2_vB7wmBC4uJ3CRr57ch6Lg1O8YAIMIbpNtkxeKcc&_nc_ht=scontent.faep9-2.fna&oh=00_AT9ue-9DjYOLBRSZnv35jscICfyZ6G21POp-47MKDRPoLA&oe=62AEE8D2',
+        imagen: 'https://scontent.faep9-2.fna.fbcdn.net/v/t39.30808-6/283214549_1214587296019542_4809080955536090293_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=a26aad&_nc_ohc=T0M6zPmruG4AX-5WG9o&_nc_ht=scontent.faep9-2.fna&oh=00_AT-gRem1ttbdmu1aUpRtG3E8N52ZqKlWncal8WoDLwU4iw&oe=62C8156A  ',
         direc: '/remeras'
 
       },
       {
         titulo: 'Camperas',
-        imagen: 'https://scontent.faep9-2.fna.fbcdn.net/v/t39.30808-6/279860122_1202046493940289_5347664077626050814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeHeyyh3HGcPZRkTbLzlVGKv3g4W89XbyeneDhbz1dvJ6Z5_SKiTUNkbb6jQDg5QtCcM4K4LPRxdO1XiVucDn-CV&_nc_ohc=_XJDcub8qKYAX9Wd4LX&_nc_ht=scontent.faep9-2.fna&oh=00_AT8pQ5JEkfK1QM-s7Q1h5ciSwj19IcXzAYsJ_cp2MT2UsA&oe=62AEF5AD',
+        imagen: 'https://scontent.faep9-2.fna.fbcdn.net/v/t39.30808-6/279860122_1202046493940289_5347664077626050814_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a26aad&_nc_ohc=YReT5TKQ9WgAX_H1jIy&_nc_ht=scontent.faep9-2.fna&oh=00_AT8I4bD2gtwiQj70admkgXJcOe96rqa9kGVjqjhKBPN5hw&oe=62C8AAED',
         direc: '/camperas'
 
       }
